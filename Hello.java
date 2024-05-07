a3c2c34e38e2987f25134c5c47fd3b686fda3dc4
jyh <568000273@qq.com>
2024-05-07 21:45:32 +0800

    dev mod

--- Hello.java
+++ Hello.java
@@ -3,6 +3,6 @@ public class Hello {
     public static void main(String[] args){
         System.out.println("hello~");
         System.out.println("hello~2");
-        System.out.println("hello~3");
+        System.out.println("dev mod");
     }
 }
